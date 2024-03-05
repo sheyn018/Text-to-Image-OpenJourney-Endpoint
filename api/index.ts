c066b3c3d99cd3820742d1832fe62886abf60ba1
sheyn018 <sheane39@gmail.com>
2024-03-05 20:25:26 +0800

    still fixing

--- api/index.ts
+++ api/index.ts
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+ 
 // Define the previous route
 // app.get('/', (req, res) => res.send('Express on Vercel'));
 
